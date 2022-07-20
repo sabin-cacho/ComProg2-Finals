847364ceea9f22ac0cb4d469244acfd1f54f3211
sabin-cacho <sabin.cacho@ciit.edu.ph>
2022-07-20 23:07:02 +0800

    checkpoint 2: kyle

--- src/com/ciit/battlesimulator/SuccubusDemon.java
+++ src/com/ciit/battlesimulator/SuccubusDemon.java
@@ -1,12 +1,13 @@
 package com.ciit.battlesimulator;
 import java.util.Random;
 
+
 public class SuccubusDemon extends Monster {
     public SuccubusDemon(){
         this.setName("Akira Demon");
         this.setHP(15);
         this.setMaxHP(25);
-        this.setDef(5);
+        this.setDef(15);
         this.setAtk(7);
         this.setMRes(3);
         this.setBRes(5);
@@ -23,11 +24,15 @@ public class SuccubusDemon extends Monster {
         int chance = rand.nextInt(10);
 
         if (chance % 2 == 0){
+            System.out.println("Akira Demon Has Possessed The Hyper Attack");
+            Thread.sleep(3000);
             int amount = player.getHP();
             player.setHP(amount / 2);
+
         }
         else{
             System.out.println("Akira Demon's Hyper Attack Failed");
         }
     }
 }
+//[MATIAS] Logs Created The Akira Demon.
