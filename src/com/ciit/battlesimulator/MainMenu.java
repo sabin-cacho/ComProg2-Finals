0292728892b2fe62a2024bee8bfcd3f772b5d672
sabin-cacho <sabin.cacho@ciit.edu.ph>
2022-07-21 00:38:03 +0800

    checkpoint 3: 75% done (nate)

--- src/com/ciit/battlesimulator/MainMenu.java
+++ src/com/ciit/battlesimulator/MainMenu.java
@@ -1,11 +1,19 @@
 package com.ciit.battlesimulator;
 
+import static java.lang.String.valueOf;
+
 public class MainMenu {
     Simulator game = new Simulator();
     GUI gui = new GUI();
 
     public void displayMenu() {
+        String[] options = {"Start", "Credits", "Exit"};
+        for (int i = 0; i < options.length; i++){
+            System.out.printf("%s - %s\n", valueOf(i+1), options[i]);
+        }
         System.out.println();
+
+
     }
 
     public void runSimulator() {
--- src/com/ciit/battlesimulator/Warrior.java
+++ src/com/ciit/battlesimulator/Warrior.java
@@ -1,4 +1,10 @@
 package com.ciit.battlesimulator;
 
 public class Warrior extends Player{
+    public Warrior(){
+        this.setName("Warrior");
+        this.setHP(100);
+        this.setMaxHP(100);
+        //wait what testing lang to wait what
+    }
 }
