28f9019db151026d6da11be01ebfbe118dbc729a
sabin-cacho <sabin.cacho@ciit.edu.ph>
2022-07-20 21:42:12 +0800

    code is now runnable

--- src/com/ciit/battlesimulator/Program.java
+++ src/com/ciit/battlesimulator/Program.java
@@ -11,9 +11,8 @@ public class Program {
             menu.displayMenu();
 
             System.out.print("Would you like to play again? (Y/N): ");
-            System.out.print(" "); //fixes the scanner
+
             ch = replay.nextLine().toCharArray();
-            replay.close();
         } while(Character.toLowerCase(ch[0]) == 'y');
 
         replay.close();
