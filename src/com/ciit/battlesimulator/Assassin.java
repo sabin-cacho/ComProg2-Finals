cb7cae3d2ee68fa9b15ebd2021c74b70604199b6
sabin-cacho <sabin.cacho@ciit.edu.ph>
2022-07-22 00:34:12 +0800

    checkpoint 5: 99.9% done, debugging part 2 electric boogaloo

---
+++ src/com/ciit/battlesimulator/Assassin.java
@@ -0,0 +1,8 @@
+package com.ciit.battlesimulator;
+
+public class Assassin extends Player{
+    public Assassin(){
+        this.setName("Assassin");
+        this.setSpeed(this.getSpeed()+10);
+    }
+}
